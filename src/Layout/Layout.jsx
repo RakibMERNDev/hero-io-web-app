2c3b4b5810b35669d63a0c058442f5b47a418acb
RakibMernDev <abdullahalrakib30@gmail.com>
2026-03-16 21:36:24 +0600

    added loading spinner

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/Nav/NavBar";
 
 const Layout = () => {
+  const navigation = useNavigation();
+
   return (
     <div className="max-w-400 mx-auto">
       <NavBar />
+      {navigation.state === "loading" && (
+        <span className="loading loading-spinner loading-xl min-h-[calc(100vh-24rem)] flex justify-center items-center"></span>
+      )}
       <Outlet />
       <Footer />
     </div>
